d77878ffea6fe8f9fe75d63d23256649dd2888a3
poteat <me@mpote.at>
2020-10-04 18:29:30 -0700

    Remove default export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ export { RetrospectiveFunction } from "./types/RetrospectiveFunction";
 export { RetrospectiveChain } from "./types/chain/RetrospectiveChain";
 
 export { reduceChain } from "./utility/reduction/reduceChain";
-
-export default reduceChain;
